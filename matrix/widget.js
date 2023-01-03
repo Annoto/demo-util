1c7eb75753ef5bb99f0f0ae5217456a669d299b8
genadis <genadis.pub@gmail.com>
2023-01-03 09:54:01 +0200

    fix: matrix initial demo script for youtube

--- matrix/widget.js
+++ matrix/widget.js
@@ -1,21 +1,46 @@
 (function (window) {
 
-    var bootstrapUrl = 'https://cdn.annoto.net/widget/latest/bootstrap.js'; 
- 
+    var bootstrapUrl = 'https://cdn.annoto.net/widget/latest/bootstrap.js';
+    var clientId = 'eyJhbGciOiJIUzI1NiJ9.MWY2YTMxYjMtZGM5ZC00YTcxLTgyMTUtZDliNTBiN2FlODBm.a_XO_gNYScfl0qmvDAtiZ8XKPUArcGmf7gFQ0whM1ag';
+    var playerQuery = '#YouTubePlayer';
+    var annotoConfig = {
+        clientId: clientId,
+        /* hooks: {
+            ssoAuthRequestHandle: ssoAuthRequestHandle,
+            mediaDetails: function (detailsParams) {
+                var retVal = detailsParams.details || {};
+                if (!retVal.title) {
+                    retVal.title = document.title || 'UNKNOWN';
+                }
+                return retVal;
+            },
+        }, */
+        widgets: [{
+            player: {
+                type: 'youtube',
+                element: playerQuery,
+            },
+        }],
+    };
+
+
+    function asyncLoadScript(src, onLoadCb) {
+        var t = document.createElement('script');
+        t.type = 'text/javascript'; t.async = true; t.src = src;
+        t.onload = onLoadCb;
+        var ft = document.getElementsByTagName('script')[0];
+        ft ? ft.parentNode.insertBefore(t, ft) : document.body.appendChild(t);
+    }
 
     var onWindowLoad = function () {
-       
-
+        asyncLoadScript(bootstrapUrl, function () {
+            Annoto.boot(annotoConfig);
+        });
     };
 
     if (['interactive', 'complete'].indexOf(document.readyState) !== -1) {
         onWindowLoad();
     } else {
-        if (window.addEventListener) { // Mozilla, Netscape, Firefox
-            window.addEventListener('load', onWindowLoad, false);
-        } else if (window.attachEvent) { // IE
-            window.attachEvent('onload', onWindowLoad);
-        }
+        window.addEventListener('load', onWindowLoad, false);
     }
-
 })(window);
