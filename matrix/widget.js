00af7cbb5c770bdb2142c8a6f0f84f72245303d9
ilya_razuvaev <ilya.razuvaev@annoto.net>
2023-07-27 11:37:03 +0300

    feat: Outbound url query param

--- matrix/widget.js
+++ matrix/widget.js
@@ -1,5 +1,5 @@
 (function (window) {
-
+    var scriptSrc = document.currentScript.src;
     var bootstrapUrl = 'https://cdn.annoto.net/widget/latest/bootstrap.js';
     var clientId = 'eyJhbGciOiJIUzI1NiJ9.MWY2YTMxYjMtZGM5ZC00YTcxLTgyMTUtZDliNTBiN2FlODBm.a_XO_gNYScfl0qmvDAtiZ8XKPUArcGmf7gFQ0whM1ag';
     var playerQuery = '#YouTubePlayer';
@@ -32,17 +32,31 @@
         ft ? ft.parentNode.insertBefore(t, ft) : document.body.appendChild(t);
     }
 
+    function getQueryParams(url) {
+        var paramArr = url.slice(url.indexOf('?') + 1).split('&');
+        var params = {};
+        paramArr.map(param => {
+            var [key, val] = param.split('=');
+            params[key] = decodeURIComponent(val);
+        })
+        return params;
+    }
+
+    function getOutboundUrl() {
+        var outboundUrl = getQueryParams(scriptSrc).outbound_url;
+        if (!outboundUrl) {
+            throw new Error('outboundUrl is not defined');
+        }
+        return outboundUrl;
+    }
+
     var onWindowLoad = function () {
         var playerEl = document.querySelector(playerQuery);
         if (!playerEl.src.includes('enablejsapi')) {
             playerEl.src = playerEl.src + '?enablejsapi=1';
         }
-        fetch('https://matrixsb.csod.com/outboundsso.aspx?ou_id=-24350', {
+        fetch(getOutboundUrl(), {
             method: 'GET',
-            headers:{
-                'Content-Type': 'text/html; charset=utf-8',
-            },
-            mode: 'no-cors',
         }).then(function (response) {
             return response.text();
         }).then(function (data) {
