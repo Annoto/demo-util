6a09292c14c1ac98458529be95feac6319f64f5f
ilya_razuvaev <ilya.razuvaev@annoto.net>
2023-08-01 18:07:03 +0300

    feat: Updated script for mulltiple and dynamic videos

--- matrix/widget.js
+++ matrix/widget.js
@@ -1,6 +1,6 @@
 (function (window) {
+    var scriptSrc = document.currentScript.src;
     var bootstrapUrl = 'https://cdn.annoto.net/widget/latest/bootstrap.js';
-    var playerQuery = '#YouTubePlayer';
     var annotoConfig = {
         /* hooks: {
             ssoAuthRequestHandle: ssoAuthRequestHandle,
@@ -12,68 +12,163 @@
                 return retVal;
             },
         }, */
-        widgets: [{
-            player: {
-                type: 'youtube',
-                element: playerQuery,
-            },
-        }],
+        widgets: [],
     };
 
-    var scriptSrc = document.currentScript.src;
     var paramArr = scriptSrc.slice(scriptSrc.indexOf('?') + 1).split('&');
     var queryParams = {};
-    paramArr.forEach(param => {
+    paramArr.map((param) => {
         var [key, val] = param.split('=');
         queryParams[key] = decodeURIComponent(val);
-    })
+    });
+
+    var ssoUrl = queryParams.sso_url;
+    var clientId = queryParams.client_id;
+    var region = queryParams.region;
+
+    // TEST VALUES
+    // var ssoUrl = 'https://matrixsb.csod.com/outboundsso.aspx?ou_id=-24350';
+    // var clientId =
+    //     'eyJhbGciOiJIUzI1NiJ9.MGQ0NjMwN2EtNDVlMi00MWFlLWJhODQtOGMwNWZhZTZhZTVj.zrGJG_FatEsGaU82OoWUa81mv3ljnCUlJ8IW-lst0qk';
+    // var region = 'queryParams.region';
+
+    if (!ssoUrl || !clientId || !region) {
+        throw new Error('Annoto: sso_url, client_id and region must be defined in the url');
+    }
+    annotoConfig.clientId = clientId;
+    annotoConfig.backend = {
+        domain: `${region}.annoto.net`,
+    };
+
+    var booted = false;
+    var annotoApi;
+    var activeAkamaiPlayerId;
+    var launchedPlayers = [];
+
+    function launchWidget(player) {
+        if (launchedPlayers.indexOf(player.playerId) !== -1) {
+            return;
+        }
+        var widgetConfig = {
+            clientId,
+            widgets: [
+                {
+                    timeline: {
+                        overlay: true,
+                    },
+                    player: player,
+                },
+            ],
+        };
+        launchedPlayers.push(player.playerId);
+        if (!booted) {
+            fetch(ssoUrl, {
+                method: 'GET',
+            })
+                .then(function (response) {
+                    return response.text();
+                })
+                .then(function (data) {
+                    var parser = new DOMParser();
+                    var doc = parser.parseFromString(data, 'text/html');
+                    var SAMLResponse = doc.querySelector('input[name="SAMLResponse"]').value;
+                    var bootFn = function () {
+                        Annoto.on('ready', function (api) {
+                            annotoApi = api;
+                            api.samlAuth(SAMLResponse);
+                        });
+                        Annoto.boot(widgetConfig);
+                        booted = true;
+                    };
+                    if (player.type === 'akamai') {
+                        player.element.amp.once('play', bootFn);
+                        player.element.amp.once('destroy', function () {
+                            const index = launchedPlayers.indexOf(player.playerId);
+                            if (index > -1) {
+                                launchedPlayers.splice(index, 1);
+                            }
+                        });
+                    } else {
+                        bootFn();
+                    }
+                });
+        } else {
+            if (!annotoApi) {
+                console && console.warn('AnnotoPlugin: Cannot Reload, Widget is Booting');
+                return;
+            }
+            var loadFn = function () {
+                annotoApi.load(widgetConfig);
+            };
+            if (player.type === 'akamai') {
+                player.element.amp.once('play', loadFn);
+                player.element.amp.once('destroy', function () {
+                    const index = launchedPlayers.indexOf(player.playerId);
+                    if (index > -1) {
+                        launchedPlayers.splice(index, 1);
+                    }
+                });
+            } else {
+                loadFn();
+            }
+        }
+    }
 
     function asyncLoadScript(src, onLoadCb) {
-        var t = document.createElement('script');
-        t.type = 'text/javascript'; t.async = true; t.src = src;
-        t.onload = onLoadCb;
         var ft = document.getElementsByTagName('script')[0];
+        var t = document.createElement('script');
+        t.type = 'text/javascript';
+        t.async = true;
+        t.src = src;
+        t.onload = onLoadCb;
         ft ? ft.parentNode.insertBefore(t, ft) : document.body.appendChild(t);
     }
 
     var onWindowLoad = function () {
-        var playerEl = document.querySelector(playerQuery);
-        if (!playerEl.src.includes('enablejsapi')) {
-            playerEl.src = playerEl.src + '?enablejsapi=1';
-        }
-
-        var ssoUrl = queryParams.sso_url;
-        var clientId = queryParams.client_id;
-        var region = queryParams.region;
-
-        if (!ssoUrl || !clientId || !region) {
-            throw new Error('sso_url, client_id and region must be defined in the url');
-        }
-        annotoConfig.clientId = clientId;
-        annotoConfig.backend = {
-            domain: `${region}.annoto.net`,
-        }
-        fetch(ssoUrl, {
-            method: 'GET',
-        }).then(function (response) {
-            return response.text();
-        }).then(function (data) {
-            var parser = new DOMParser();
-            var doc = parser.parseFromString(data, "text/html")
-            var SAMLResponse = doc.querySelector('input[name="SAMLResponse"]').value;
-
-            asyncLoadScript(bootstrapUrl, function () {
-                Annoto.on('ready', function (api) {
-                    api.samlAuth(SAMLResponse);
-                });
-                Annoto.boot(annotoConfig);
-            });
-        })
+        asyncLoadScript(bootstrapUrl, function () {
+            setInterval(function () {
+                var youtubeElements = document.querySelectorAll('iframe[src*="youtube.com"]'),
+                    akamaiElements = document.querySelectorAll('.akamai-player');
+                if (akamaiElements.length + youtubeElements.length === 0) {
+                    activeAkamaiPlayerId = null;
+                    return;
+                }
+                for (var i = 0; i < youtubeElements.length; i++) {
+                    var youtube = youtubeElements[i];
+                    if (!youtube.id || youtube.id === '') {
+                        youtube.id =
+                            'annoto_player_Y_id_' + Math.random().toString(36).substr(2, 6);
+                    }
+                    launchWidget({
+                        playerId: `#${youtube.id}`,
+                        type: 'youtube',
+                        element: youtube,
+                    });
+                }
+                for (var j = 0; j < akamaiElements.length; j++) {
+                    var akamai = akamaiElements[j];
+                    if (!akamai.id || akamai.id === '') {
+                        akamai.id = 'annoto_player_A_id_' + Math.random().toString(36).substr(2, 6);
+                    }
+                    launchWidget({
+                        playerId: `#${akamai.id}`,
+                        type: 'akamai',
+                        element: akamai,
+                    });
+                }
+            }, 200);
+        });
     };
 
     if (['interactive', 'complete'].indexOf(document.readyState) !== -1) {
         onWindowLoad();
     } else {
-        window.addEventListener('load', onWindowLoad, false);
+        if (window.addEventListener) {
+            // Mozilla, Netscape, Firefox
+            window.addEventListener('load', onWindowLoad, false);
+        } else if (window.attachEvent) {
+            // IE
+            window.attachEvent('onload', onWindowLoad);
+        }
     }
 })(window);
